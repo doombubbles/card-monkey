ce7c00f07bcdc3622ed5e5b8911601870bd276eb
doombubbles <obsidiantoolzg@gmail.com>
2021-07-03 23:37:06 -0700

    Card Monkey Pre-Release

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using BTD_Mod_Helper;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(CardMonkey.Main), "Card Monkey", "0.1.0", "doombubbles")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace CardMonkey
+{
+    public class Main : BloonsTD6Mod
+    {
+        public override string MelonInfoCsURL =>
+            "https://github.com/doombubbles/card-monkey/main/Main.cs";
+
+        public override string LatestURL =>
+            "https://github.com/doombubbles/card-monkey/blob/main/CardMonkey.dll?raw=true";
+        
+        // No Harmony Patches or hooks required for this whole tower!
+    }
+}
